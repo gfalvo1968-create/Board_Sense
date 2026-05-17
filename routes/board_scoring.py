c95248d3ce6c79a8f828d74e85554471a4d43b36
gfalvo1968-create <gfalvo1968@icloud.com>
2026-05-17 05:34:48 -0400

    Refactor calculate_score function for feature scoring

--- routes/board_scoring.py
+++ routes/board_scoring.py
@@ -1,23 +1,24 @@
-def calculate_score(features):
+# routes/board_scoring.py
 
+def calculate_score(features):
     score = 0
 
     if features.get("motherboard"):
         score += 5
 
-    if features.get("ram"):
-        score += 4
+    if features.get("memory_module") or features.get("ram"):
+        score += 5
+
+    if features.get("gold_fingers"):
+        score += 3
+
+    if features.get("large_ic_chips"):
+        score += 3
+
+    if features.get("processor"):
+        score += 7
 
     if features.get("power_board"):
         score -= 3
 
     return max(score, 0)
-
-if features["memory_module"]:
-    score += 5
-
-if features["gold_fingers"]:
-    score += 3
-
-if features["large_ic_chips"]:
-    score += 3
