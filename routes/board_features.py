c2d8fb8f5c1696ca868c3860f846a4e08438bfc1
gfalvo1968-create <gfalvo1968@icloud.com>
2026-05-16 20:19:16 -0400

    Add function to detect board features from image path

--- routes/board_features.py
+++ routes/board_features.py
@@ -1,1 +1,9 @@
+def detect_board_features(image_path):
 
+    lower_name = image_path.lower()
+
+    return {
+        "motherboard": "motherboard" in lower_name,
+        "ram": "ram" in lower_name,
+        "power_board": "power" in lower_name
+    }
